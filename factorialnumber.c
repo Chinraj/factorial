4c961124924959a9c56cffc8538beae9f2c7fdc2
Chinraj.K <chinrajkalimuthu@gmail.com>
2016-06-28 11:54:40 +0530

    Update factorialnumber.c

--- factorialnumber.c
+++ factorialnumber.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
+#include<conio.h>
 void main()
 {
    int a,b,f=1;
    clrscr();
-    printf("Enter a number : ");
+    printf("Integer number : ");
     scanf("%d",&a);
     for(b=1; b<=a; b++)
     {
    f=f*b;
     }
-    printf("Factorial of the number %d = %d",a,f);
+    printf("The Factorial number is %d = %d",a,f);
    getch();
 }
